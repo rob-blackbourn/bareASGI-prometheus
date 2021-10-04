378fb131d9491dcecb7cccf07fb81af34021a8ce
Rob Blackbourn <rob.blackbourn@googlemail.com>
2021-10-04 08:51:20 +0100

    Fixed the scope

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -4,11 +4,7 @@ from time import sleep
 
 from jetblack_metrics import monitor
 
-from baretypes import (
-    Scope,
-    Info,
-    RouteMatches
-)
+from bareasgi import HttpRequest
 
 from bareasgi_prometheus.metrics import HttpRequestMetric
 
@@ -18,15 +14,14 @@ def test_http_request_metric():
 
     host = 'host'
     app_name = 'test'
-    scope: Scope = {'method': 'GET', 'path': '/index.html'}
-    info: Info = {}
-    matches: RouteMatches = {}
-    metric = HttpRequestMetric(host, app_name, scope, info, matches)
+    scope = {'method': 'GET', 'path': '/index.html'}
+    request = HttpRequest(scope, {}, {}, {}, None)  # type: ignore
+    metric = HttpRequestMetric(host, app_name, request)
     with monitor(metric):
         sleep(1)
         metric.status = 200
     assert metric.error is None
     assert round(metric.elapsed) == 1
     assert metric.status == 200
-    assert metric.scope == scope
+    assert metric.request.scope == scope
     assert metric.app_name == app_name
